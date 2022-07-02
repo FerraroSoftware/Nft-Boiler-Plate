4967aa1fc544e1f313376fade3da451da88c2511
FerraroSoftware <software.ferraro@gmail.com>
2022-07-02 13:46:09 -0400

    bring in images and start updating minting section with header

--- minting-dapp/src/scripts/main.tsx
+++ minting-dapp/src/scripts/main.tsx
@@ -3,8 +3,10 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import ReactDOM from 'react-dom';
 import Dapp from './react/Dapp';
+import Hero from './react/Hero';
 import Header from './react/Header';
 import CollectionConfig from '../../../smart-contract/config/CollectionConfig';
+import NetworkConfigInterface from '../../../smart-contract/lib/NetworkConfigInterface';
 import { ToastContainer } from 'react-toastify';
 
 if (document.title === '') {
@@ -20,6 +22,17 @@ document.addEventListener('DOMContentLoaded', async () => {
           pauseOnHover={true}
           theme='light' />
     <Header></Header>
+    {/* <Hero networkConfig={this.state.networkConfig}
+                    maxSupply={this.state.maxSupply}
+                    totalSupply={this.state.totalSupply}
+                    tokenPrice={this.state.tokenPrice}
+                    maxMintAmountPerTx={this.state.maxMintAmountPerTx}
+                    isPaused={this.state.isPaused}
+                    isWhitelistMintEnabled={this.state.isWhitelistMintEnabled}
+                    isUserInWhitelist={this.state.isUserInWhitelist}
+                    mintTokens={(mintAmount) => this.mintTokens(mintAmount)}
+                    whitelistMintTokens={(mintAmount) => this.whitelistMintTokens(mintAmount)}
+                    loading={this.state.loading}></Hero> */}
     <Dapp />
   </>, document.getElementById('minting-dapp'));
 });
--- minting-dapp/src/scripts/react/MintWidget.tsx
+++ minting-dapp/src/scripts/react/MintWidget.tsx
@@ -1,6 +1,6 @@
-import { utils, BigNumber } from 'ethers';
-import React from 'react';
-import NetworkConfigInterface from '../../../../smart-contract/lib/NetworkConfigInterface';
+import { utils, BigNumber } from "ethers";
+import React from "react";
+import NetworkConfigInterface from "../../../../smart-contract/lib/NetworkConfigInterface";
 
 interface Props {
   networkConfig: NetworkConfigInterface;
@@ -41,7 +41,10 @@ export default class MintWidget extends React.Component<Props, State> {
 
   private incrementMintAmount(): void {
     this.setState({
-      mintAmount: Math.min(this.props.maxMintAmountPerTx, this.state.mintAmount + 1),
+      mintAmount: Math.min(
+        this.props.maxMintAmountPerTx,
+        this.state.mintAmount + 1
+      ),
     });
   }
 
@@ -64,35 +67,202 @@ export default class MintWidget extends React.Component<Props, State> {
   render() {
     return (
       <>
-      {/* need to check if  */}
-        {this.canMint() ?
-          <div className={`mint-widget ${this.props.loading ? 'animate-pulse saturate-0 pointer-events-none' : ''}`}>
+        {/* Illustration behind hero content */}
+        <div
+          className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none"
+          aria-hidden="true"
+        ></div>
+
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          {/* Hero content */}
+          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+            {/* Section header */}
+            <div className="text-center pb-12 md:pb-16">
+              <h1
+                className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4 text-white"
+                data-aos="zoom-y-out"
+              >
+                Join the Fox{" "}
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-amber-600 to-orange-300">
+                  Sanctuary
+                </span>
+              </h1>
+              <div className="max-w-3xl mx-auto">
+                <p
+                  className="text-xl text-gray-300 mb-8"
+                  data-aos="zoom-y-out"
+                  data-aos-delay="150"
+                >
+                  The foxes are hiding out in the den. They are looking for a
+                  new owner to help them survive in the metaverse.
+                </p>
+                <div
+                  className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center"
+                  data-aos="zoom-y-out"
+                  data-aos-delay="300"
+                >
+                  {!this.canMint() ? (
+                    <div
+                      className={`mint-widget ${
+                        this.props.loading
+                          ? "animate-pulse saturate-0 pointer-events-none"
+                          : ""
+                      }`}
+                    >
+                      {/* what we preview before minting the NFT i think
+          <div className="preview">
+            <img src="/build/images/preview.png" alt="Collection preview" />
+          </div> */}
+
+                      <div className="price">
+                        <strong>Total price:</strong>{" "}
+                        {utils.formatEther(
+                          this.props.tokenPrice.mul(this.state.mintAmount)
+                        )}{" "}
+                        {this.props.networkConfig.symbol}
+                      </div>
+
+                      {/* here we control how many we mint and the mint button */}
+                      <div className="controls">
+                        <button
+                          className="decrease"
+                          disabled={this.props.loading}
+                          onClick={() => this.decrementMintAmount()}
+                        >
+                          -
+                        </button>
+                        <span className="mint-amount">
+                          {this.state.mintAmount}
+                        </span>
+                        <button
+                          className="increase"
+                          disabled={this.props.loading}
+                          onClick={() => this.incrementMintAmount()}
+                        >
+                          +
+                        </button>
+                      </div>
+                      <button
+                        className="primary"
+                        disabled={this.props.loading}
+                        onClick={() => this.mint()}
+                      >
+                        Mint
+                      </button>
+
+                      <div>
+                        <button
+                          className="bg-orange-500 text-white px-4 py-2 mx-2 rounded-md text-1xl font-medium hover:bg-orange-700 transition duration-30"
+                          disabled={this.props.loading}
+                          onClick={() => this.mint()}
+                        >
+                          Mint
+                        </button>
+                      </div>
+                    </div>
+                  ) : (
+                    <>
+                      <div>
+                        <a
+                          className="bg-orange-500 text-white px-4 py-2 mx-2 rounded-md text-1xl font-medium hover:bg-orange-700 transition duration-30"
+                          href="#0"
+                        >
+                          OpenSea
+                        </a>
+                      </div>
+                      <div>
+                        <a
+                          className="mx-2 border-2 border-orange-500 text-white px-4 py-2 rounded-md text-1xl font-medium hover:bg-orange-500 transition duration-300"
+                          href="#0"
+                        >
+                          Contract
+                        </a>
+                      </div>
+                    </>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            {/* Hero image */}
+            <div>
+              <div className="relative flex  mb-8">
+                <div className="flex flex-col ">
+                  <img
+                    className="mx-auto"
+                    src={"/build/images/woods2.svg"}
+                    alt="Hero"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* need to check if  */}
+        {this.canMint() ? (
+          <div
+            className={`mint-widget ${
+              this.props.loading
+                ? "animate-pulse saturate-0 pointer-events-none"
+                : ""
+            }`}
+          >
             {/* what we preview before minting the NFT i think */}
             <div className="preview">
               <img src="/build/images/preview.png" alt="Collection preview" />
             </div>
 
             <div className="price">
-              <strong>Total price:</strong> {utils.formatEther(this.props.tokenPrice.mul(this.state.mintAmount))} {this.props.networkConfig.symbol}
+              <strong>Total price:</strong>{" "}
+              {utils.formatEther(
+                this.props.tokenPrice.mul(this.state.mintAmount)
+              )}{" "}
+              {this.props.networkConfig.symbol}
             </div>
 
-          {/* here we control how many we mint and the mint button */}
+            {/* here we control how many we mint and the mint button */}
             <div className="controls">
-              <button className="decrease" disabled={this.props.loading} onClick={() => this.decrementMintAmount()}>-</button>
+              <button
+                className="decrease"
+                disabled={this.props.loading}
+                onClick={() => this.decrementMintAmount()}
+              >
+                -
+              </button>
               <span className="mint-amount">{this.state.mintAmount}</span>
-              <button className="increase" disabled={this.props.loading} onClick={() => this.incrementMintAmount()}>+</button>
-              <button className="primary" disabled={this.props.loading} onClick={() => this.mint()}>Mint</button>
+              <button
+                className="increase"
+                disabled={this.props.loading}
+                onClick={() => this.incrementMintAmount()}
+              >
+                +
+              </button>
+              <button
+                className="primary"
+                disabled={this.props.loading}
+                onClick={() => this.mint()}
+              >
+                Mint
+              </button>
             </div>
           </div>
-          :
+        ) : (
           <div className="cannot-mint">
             {/* check out that juicy emoji! */}
             <span className="emoji">⏳</span>
-
-            {this.props.isWhitelistMintEnabled ? <>You are not included in the <strong>whitelist</strong>.</> : <>The contract is <strong>paused</strong>.</>}<br />
+            {this.props.isWhitelistMintEnabled ? (
+              <>
+                You are not included in the <strong>whitelist</strong>.
+              </>
+            ) : (
+              <>
+                The contract is <strong>paused</strong>.
+              </>
+            )}
+            <br />
             Please come back during the next sale!
           </div>
-        }
+        )}
       </>
     );
   }
