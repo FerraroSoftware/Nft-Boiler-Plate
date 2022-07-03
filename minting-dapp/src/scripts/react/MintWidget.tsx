cf9684b1563002579ed4599911b0fc500a49ebc6
FerraroSoftware <software.ferraro@gmail.com>
2022-07-03 19:18:09 -0400

    play with button colors

--- minting-dapp/src/scripts/react/MintWidget.tsx
+++ minting-dapp/src/scripts/react/MintWidget.tsx
@@ -103,7 +103,7 @@ export default class MintWidget extends React.Component<Props, State> {
                 >
                   {this.canMint() ? (
                     <div
-                      className={`flex flex-col items-center rounded-lg overflow-hidden bg-orange-500 text-white shadow p-4 ${
+                      className={`flex flex-col items-center rounded-lg overflow-hidden bg-green-600 text-white shadow p-4 ${
                         this.props.loading
                           ? "animate-pulse saturate-0 pointer-events-none"
                           : ""
@@ -146,7 +146,7 @@ export default class MintWidget extends React.Component<Props, State> {
 
                       <div>
                         <button
-                          className="bg-orange-300 text-white px-4 py-2 mx-2 rounded-md text-1xl font-medium hover:bg-orange-500 transition duration-30"
+                          className="text-white bg-gradient-to-br from-amber-600 to-orange-300 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                           disabled={this.props.loading}
                           onClick={() => this.mint()}
                         >
